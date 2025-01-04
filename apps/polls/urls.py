150a143355ab16309cba0a71e7d86aeaf32a8444
kkig <kigoshi.kanazawa@gmail.com>
2025-01-04 21:54:23 +0900

    Update detail page of polls app

--- apps/polls/urls.py
+++ apps/polls/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     # /polls/
     path("", views.index, name="index"),
-    # /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+    # /polls/specifics/5/
+    path("specifics/<int:question_id>/", views.detail, name="detail"),
     # /polls/5/results/
     path("<int:question_id>/results/", views.results, name="results"),
     # /polls/5/vote/
