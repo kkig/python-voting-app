bc4da39e6b24979a215b22eff363c17e350d09f4
kkig <kigoshi.kanazawa@gmail.com>
2025-01-06 23:20:02 +0900

    Added test for was_published_recently() and views

--- apps/polls/views.py
+++ apps/polls/views.py
@@ -7,6 +7,7 @@ from django.db.models import F
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 # from django.template import loader
 
@@ -18,8 +19,13 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        """Return the last five published questions (not including those set to be
+        published in the future)."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
+
+        # return Question.objects.order_by("-pub_date")[:5]
 
 
 # def index(request):
